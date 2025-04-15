e0e247eb115403aeb16d4053306887918562531f
SIIT_Tae <6522780400@g.siit.tu.ac.th>
2025-04-15 21:08:50 +0700

    feat: transformData service

--- src/api/transformData.controller.ts
+++ src/api/transformData.controller.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import { transformData } from "../services/transformData.service";
 
 const router = Router();
 
 router.get("/transformData", async (req, res) => {
   try {
-    res.json({ test: "EiEi" });
+    const result = await transformData();
+    res.json(result);
   } catch (error) {
     res.status(500).json({ message: "Internal server error", error });
   }
---
+++ src/services/transformData.service.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { TransformedDataType, User } from "../utils/types";
+
+export async function transformData(): Promise<TransformedDataType> {
+  const { data } = await axios.get("https://dummyjson.com/users");
+  const users: User[] = data.users;
+
+  const groupUsers: TransformedDataType = {};
+
+  for (const user of users) {
+    const department = user.company.department;
+
+    if (!groupUsers[department]) {
+      groupUsers[department] = {
+        male: 0,
+        female: 0,
+        ageRange: "",
+        hair: {},
+        addressUser: {},
+      };
+    }
+
+    const groupUser = groupUsers[department];
+    user.gender === "male" ? groupUser.male++ : groupUser.female++;
+
+    const hairColor = user.hair.color;
+    groupUser.hair[hairColor] = (groupUser.hair[hairColor] || 0) + 1;
+
+    const addressUserName = `${user.firstName}${user.lastName}`;
+    groupUser.addressUser[addressUserName] = user.address.postalCode;
+  }
+
+  for (const department in groupUsers) {
+    const departmentUsers = users.filter(
+      (user) => user.company.department === department
+    );
+    const ages = departmentUsers.map((user) => user.age);
+    const minAge = Math.min(...ages);
+    const maxAge = Math.max(...ages);
+    groupUsers[department].ageRange = `${minAge}-${maxAge}`;
+  }
+
+  return groupUsers;
+}
